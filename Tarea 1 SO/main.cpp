321348d9fe2161263515c431d08bc541d87ac8f5
benja_acd <bcastro@utem.cl>
2022-05-28 21:18:23 -0400

    SJFA apropiativo

--- Tarea 1 SO/main.cpp
+++ Tarea 1 SO/main.cpp
@@ -81,6 +81,47 @@ void FCFS(job j1[], int n)
         cout << j1[i].getID()<<" | tt = "<<j1[i].getts()-j1[i].getTLL()<<"| wt = "<<j1[i].getta()-j1[i].getTLL()<<"|"<<endl;
     }
 }
+void SJFA(job j1[], int n)
+{
+    int pos, ver = 0,ver2 = 0;
+    float taux, tsaux=0,tllaux;
+    for (int i = 0; i < n; i++)
+    {
+        float aux = 10000,aux2=10000;
+        for (int i = 0; i < n; i++)
+        {
+            if (j1[i].getTLL() < aux && j1[i].getver() != 0 && ver2 == 0) { aux = j1[i].getTLL(); pos = i; ver2 == 1; }
+            else if (j1[i].getTLL() < tsaux && j1[i].getTR() <= aux2 && j1[i].getver() != 0 && ver2!=0) 
+            {
+                if (j1[i].getTR() == aux2)
+                {
+                    if (j1[i].getTLL() < tllaux){ aux2 = j1[i].getTR(); pos = i; tllaux = j1[i].getTLL(); }
+                }
+                else 
+                {
+                    aux2 = j1[i].getTR(); pos = i; tllaux = j1[i].getTLL();
+                }
+                
+            }
+        }
+        if (ver == 0) {
+            taux = j1[pos].getta(); cout << taux;
+            cout << "|" << j1[pos].getID(); tsaux = taux + j1[pos].getTR(); j1[pos].setts(tsaux);
+            cout << "|" << tsaux; ver = 1;
+        }
+        else
+        {
+            taux = tsaux; j1[pos].setta(taux);  tsaux = taux + j1[pos].getTR(); j1[pos].setts(tsaux);
+            cout << "|" << j1[pos].getID();  cout << "|" << tsaux;
+        }
+        j1[pos].setver(0);
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << j1[i].getID() << " | tt = " << j1[i].getts() - j1[i].getTLL() << "| wt = " << j1[i].getta() - j1[i].getTLL() << "|" << endl;
+    }
+}
 int main()
 {
     Tlista lista = NULL;
@@ -102,5 +143,5 @@ int main()
         j1[i] = q->jobs;
         q = q->sgte;
     }
-    FCFS(j1, n);
+    SJFA(j1, n);
 }
